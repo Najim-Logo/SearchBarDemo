459cea7a860f49f26e0607aeca3196ae460a6aeb
Najim-Logo <46739670+Najim-Logo@users.noreply.github.com>
2019-02-08 23:05:27 +0530

    Add project files.

---
+++ SearchBarDemo/SearchBarDemo/ViewModels/MainViewModels.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SearchBarDemo.ViewModels
+{
+    public class MainViewModels
+    {
+
+       List<string> Country = new List<string>
+        {
+            "India",
+            "pakistan",
+            "Srilanka",
+            "Bangladesh",
+            "Afghanistan"
+        };
+
+        private List<string> countryList;
+
+        public List<string> CountryList
+        {
+            get { return countryList; }
+            set { countryList = value; }
+        }
+
+
+        private bool isVisible;
+
+        public bool IsVisible
+        {
+            get { return isVisible; }
+            set { isVisible = value; }
+        }
+
+        private string searchContent;
+
+        public string SearchContent
+        {
+            get { return searchContent; }
+            set { searchContent = value; }
+        }
+
+
+
+        private ICommand _searchCommand;
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return _searchCommand ?? (_searchCommand = new Command<string>((text) =>
+                {
+                    var keyword = text;
+                    if (keyword.Length >= 1)
+                    {
+                        CountryList = Country.Where(c => c.ToLower().Contains(keyword.ToLower())).ToList();
+                        IsVisible = true;
+                    }
+                    else
+                    {
+                        IsVisible = false;
+                    }
+                    // The text parameter can now be used for searching.
+                }));
+            }
+        }
+    }
+}
---
+++ SearchBarDemo/SearchBarDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SearchBarDemo.ViewModels;
+using Xamarin.Forms;
+
+namespace SearchBarDemo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModels();
+        }
+    }
+}
